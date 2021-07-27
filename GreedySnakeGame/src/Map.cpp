c105aaf0015b39f0c76b14e8c179f30efef278b2
vvbiscuit <1049477107@qq.com>
2021-07-27 23:21:22 +0800

    [GreedySnakeGame][feature]修改x，y坐标的关系，使得地图呈现正方形。

--- GreedySnakeGame/src/Map.cpp
+++ GreedySnakeGame/src/Map.cpp
@@ -17,15 +17,17 @@ Map::~Map()
 
 void Map::drawMap()
 {
+	//绘制地图
+	// 注意列乘以2的原因，是因为控制台里y=2x才能绘制出正方形，一个纵坐标的值等于两个横坐标的值
 	for (int row = 0; row < m_mapLength; row++)//行
 	{
-		for (int col = 0; col < m_mapLength; col++)//列
+		for (int col = 0; col < m_mapLength * 2; col++)//列
 		{
 			if (row == 0 || (row == m_mapLength - 1))//首行和最后一行
 			{
 				cout << "$";
 			}
-			else if (col == 0 || (col == m_mapLength - 1))
+			else if (col == 0 || (col == m_mapLength * 2 - 1))
 			{
 				cout << "$";
 			}
@@ -34,7 +36,7 @@ void Map::drawMap()
 				cout << " ";
 			}
 
-			if (col == m_mapLength - 1)
+			if (col == m_mapLength * 2 - 1)
 			{
 				cout << endl;
 			}
